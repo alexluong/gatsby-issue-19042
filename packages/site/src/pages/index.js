bb9bd5f990044f90d97f6a5fb2aeb81f8c594718
Alex Luong <alex@alexluong.com>
2019-10-26 04:38:51 -0500

    commit

---
+++ packages/site/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/core";
+
+function IndexPage() {
+  return (
+    <Box>
+      <Text>Hello World</Text>
+    </Box>
+  );
+}
+
+export default IndexPage;
